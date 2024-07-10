6c7653cde74e589afdfa081ccc78b2f49d2a4ef3
ChristopherS24 <chryskies@gmail.com>
2024-07-10 14:14:23 +0200

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ console.log('nome:', name);
 const surname = prompt('Quale è il tuo cognome?');
 console.log('cognome:', surname);
 
+const color = prompt('Quale è il tuo colore preferito?');
+console.log('colore preferito:', color);
+
+document.getElementById('info').innerHTML= name + surname + color + 130;
